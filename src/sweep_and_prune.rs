e54596b3eb6fcc5585131d918df83956ea3f69b9
amiani <amianijohns@gmail.com>
2023-02-16 16:31:41 -0500

    rename box to volume

--- src/sweep_and_prune.rs
+++ src/sweep_and_prune.rs
@@ -3,24 +3,26 @@ use crate::{succinct_tree::{Set, SuccinctTree}, radix_sort::RadixSort};
 
 type Real = f64;
 
-pub trait Aabb {
+pub trait Volume {
 	fn project_x(&self) -> (Real, Real);
 	fn project_y(&self) -> (Real, Real);
 	fn project_z(&self) -> (Real, Real);
 }
 
-pub fn sweep_and_prune<A: Aabb>(boxes: &[A]) -> Vec<(&A, &A)> {
-	let extents_x: Vec<(_, _)> = boxes.iter()
-		.map(Aabb::project_x)
+/// Finds collisions between bounding volumes
+/// 
+pub fn sweep_and_prune<V: Volume>(volumes: &[V]) -> Vec<(&V, &V)> {
+	let extents_x: Vec<(_, _)> = volumes.iter()
+		.map(Volume::project_x)
 		.collect();
 	let sorted_indexes_x = unzip_extents(&extents_x).argsort();
-	let boundaries_x = find_boundaries(&sorted_indexes_x);
+	let candidate_bounds_x = find_candidates(&sorted_indexes_x);
 
-	let extents_y: Vec<(_, _)> = boxes.iter()
-		.map(Aabb::project_y)
+	let extents_y: Vec<(_, _)> = volumes.iter()
+		.map(Volume::project_y)
 		.collect();
 	let sorted_indexes_y = unzip_extents(&extents_y).argsort();
-	find_pairs(&sorted_indexes_y, &boundaries_x, boxes)
+	find_pairs(&sorted_indexes_y, &candidate_bounds_x, volumes)
 }
 
 fn unzip_extents(extents: &[(Real, Real)]) -> Vec<Real> {
@@ -46,39 +48,39 @@ impl CandidateBounds {
 	}
 }
 
-fn find_boundaries(indexes: &[usize]) -> CandidateBounds {
-	let num_boxes = indexes.len() / 2;
-	let mut active_boxes = Set::new(num_boxes as u32);
+fn find_candidates(indexes: &[usize]) -> CandidateBounds {
+	let num_volumes = indexes.len() / 2;
+	let mut active_volumes = Set::new(num_volumes as u32);
 	let mut rank = 0;
-	let mut bounds = CandidateBounds::new(num_boxes);
+	let mut bounds = CandidateBounds::new(num_volumes);
 	for &index in indexes {
-		if index < num_boxes {
-			let box_id = index;
-			bounds.ranks[box_id] = rank;
-			bounds.ranks_inv[rank] = box_id;
-			let box_rank = rank;
-			active_boxes.insert(box_rank);
-			bounds.lower[box_id] = active_boxes.min().unwrap();	//TODO: remove this unwrap?
+		if index < num_volumes {
+			let volume_id = index;
+			bounds.ranks[volume_id] = rank;
+			bounds.ranks_inv[rank] = volume_id;
+			let volume_rank = rank;
+			active_volumes.insert(volume_rank);
+			bounds.lower[volume_id] = active_volumes.min().unwrap();	//TODO: remove this unwrap?
 			rank += 1;
 		} else {
-			let box_id = index - num_boxes;
-			bounds.upper[box_id] = rank;
-			let box_rank = bounds.ranks[box_id];
-			active_boxes.remove(box_rank);
+			let volume_id = index - num_volumes;
+			bounds.upper[volume_id] = rank;
+			let volume_rank = bounds.ranks[volume_id];
+			active_volumes.remove(volume_rank);
 		}
 	}
 	bounds
 }
 
-fn find_pairs<'a, A: Aabb>(sorted_indexes: &[usize], bounds: &CandidateBounds, boxes: &'a [A]) -> Vec<(&'a A, &'a A)> {
-	let num_boxes = sorted_indexes.len() / 2;
-	let mut active_ranks = Set::new(num_boxes as u32);
-	let mut pairs = Vec::<(&A, &A)>::new();
+fn find_pairs<'a, V: Volume>(sorted_indexes: &[usize], bounds: &CandidateBounds, volumes: &'a [V]) -> Vec<(&'a V, &'a V)> {
+	let num_volumes = sorted_indexes.len() / 2;
+	let mut active_ranks = Set::new(num_volumes as u32);
+	let mut pairs = Vec::<(&V, &V)>::new();
 	for &index in sorted_indexes {
-		if index < num_boxes {
-			let object = &boxes[index];
-			let are_colliding = |right: &&A| {
-				let left_extent = object.project_z();
+		if index < num_volumes {
+			let left = &volumes[index];
+			let are_colliding = |right: &&V| {
+				let left_extent = left.project_z();
 				let right_extent = right.project_z();
 				left_extent.1 > right_extent.0 && left_extent.0 < right_extent.1
 			};
@@ -86,15 +88,15 @@ fn find_pairs<'a, A: Aabb>(sorted_indexes: &[usize], bounds: &CandidateBounds, b
 			let candidates = active_ranks.range(bounds.lower[index], bounds.upper[index]);
 			let colliding_objects = candidates.iter()
 				.map(|&c| bounds.ranks_inv[c])
-				.map(|index| &boxes[index])
+				.map(|index| &volumes[index])
 				.filter(are_colliding)
-				.map(|colliding_object| (object, colliding_object));
+				.map(|colliding_volume| (left, colliding_volume));
 			pairs.extend(colliding_objects);
 
 			let rank = bounds.ranks[index];
 			active_ranks.insert(rank);
 		} else {
-			let rank = bounds.ranks[index - num_boxes];
+			let rank = bounds.ranks[index - num_volumes];
 			active_ranks.remove(rank);
 		}
 	}
@@ -105,14 +107,14 @@ fn find_pairs<'a, A: Aabb>(sorted_indexes: &[usize], bounds: &CandidateBounds, b
 mod tests {
 	use super::*;
 	
-	struct BoundingBox {
+	struct BoundingVolume {
 		position: (f64, f64, f64),
 		width: f64,
 		length: f64,
 		height: f64
 	}
 
-	impl Aabb for BoundingBox {
+	impl Volume for BoundingVolume {
 		fn project_x(&self) -> (f64, f64) {
 			(self.position.0, self.position.0 + self.width)
 		}
@@ -125,15 +127,15 @@ mod tests {
 	}
 
 	#[test]
-	fn it_finds_two_colliding_boxes() {
-		let boxes = [
-			BoundingBox {
+	fn it_finds_two_colliding_volumes() {
+		let volumes = [
+			BoundingVolume {
 				position: (1.0, 2.0, 0.0),
 				width: 1.0,
 				length: 1.0,
 				height: 1.0,
 			},
-			BoundingBox {
+			BoundingVolume {
 				position: (1.5, 2.5, 0.0),
 				width: 1.0,
 				length: 1.0,
@@ -141,9 +143,9 @@ mod tests {
 			},
 		];
 
-		let colliding_pairs = sweep_and_prune(&boxes);
+		let colliding_pairs = sweep_and_prune(&volumes);
 
-		assert!(std::ptr::eq(&boxes[0], colliding_pairs[0].1));
-		assert!(std::ptr::eq(&boxes[1], colliding_pairs[0].0));
+		assert!(std::ptr::eq(&volumes[0], colliding_pairs[0].1));
+		assert!(std::ptr::eq(&volumes[1], colliding_pairs[0].0));
 	}
 }
